2bad9dbc95d0e02cb2a19a453b5e22cce07ecedf
Sioud <sioud.zied.11@gmail.com>
2024-03-14 21:48:35 +0100

    console logs removed Added Custom Token API

--- src/Redux/QuerryAPI.jsx
+++ src/Redux/QuerryAPI.jsx
@@ -2,7 +2,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const API_TOKEN = import.meta.env.VITE_API_TOKEN;
-console.log('API_TOKEN:', API_TOKEN);
 // Define a service using a base URL and expected endpoints
 export const querryApi = createApi({
   reducerPath: 'querryApi',
@@ -13,7 +12,6 @@ export const querryApi = createApi({
     }),
   }),
 })
-console.log('API configuration:', querryApi);
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
